516234deb580411998b1c8a72245887008454a6b
Andrii Litvinov <andreoyy.litvinovv@gmail.com>
2023-10-18 17:31:20 +0300

    Assets added

--- Fighting/Controls/CharacterBox.Designer.cs
+++ Fighting/Controls/CharacterBox.Designer.cs
@@ -28,16 +28,46 @@
         /// </summary>
         private void InitializeComponent()
         {
+            CharacterNameLabel = new Label();
+            CharacterPictureBox = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)CharacterPictureBox).BeginInit();
             SuspendLayout();
+            // 
+            // CharacterNameLabel
+            // 
+            CharacterNameLabel.AutoSize = true;
+            CharacterNameLabel.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            CharacterNameLabel.Location = new Point(70, 185);
+            CharacterNameLabel.Name = "CharacterNameLabel";
+            CharacterNameLabel.Size = new Size(68, 28);
+            CharacterNameLabel.TabIndex = 0;
+            CharacterNameLabel.Text = "Name";
+            CharacterNameLabel.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // CharacterPictureBox
+            // 
+            CharacterPictureBox.Location = new Point(0, 0);
+            CharacterPictureBox.Name = "CharacterPictureBox";
+            CharacterPictureBox.Size = new Size(210, 182);
+            CharacterPictureBox.TabIndex = 1;
+            CharacterPictureBox.TabStop = false;
             // 
             // CharacterBox
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(CharacterPictureBox);
+            Controls.Add(CharacterNameLabel);
             Name = "CharacterBox";
+            Size = new Size(210, 215);
+            ((System.ComponentModel.ISupportInitialize)CharacterPictureBox).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label CharacterNameLabel;
+        private PictureBox CharacterPictureBox;
     }
 }
