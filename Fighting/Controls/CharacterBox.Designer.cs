5f1fcde93b009afda855fb38f3731558d405df12
Andrii Litvinov <andreoyy.litvinovv@gmail.com>
2023-10-18 16:58:56 +0300

    New control

---
+++ Fighting/Controls/CharacterBox.Designer.cs
@@ -0,0 +1,43 @@
+﻿namespace Fighting.Controls
+{
+    partial class CharacterBox
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            SuspendLayout();
+            // 
+            // CharacterBox
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            Name = "CharacterBox";
+            ResumeLayout(false);
+        }
+
+        #endregion
+    }
+}
