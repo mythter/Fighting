0c5e87da5b614a67ccfa301f56ee91636ac6ed57
Anton <anton260903@gmail.com>
2023-10-18 22:50:08 +0300

    characters generator class fixed

--- Fighting/Helpers/CharacterGenerator.cs
+++ Fighting/Helpers/CharacterGenerator.cs
@@ -1,4 +1,6 @@
-﻿using Fighting.Models;
+﻿using Fighting.Enums;
+using Fighting.Models;
+using System.Reflection;
 
 namespace Fighting.Helpers
 {
@@ -6,80 +8,93 @@ namespace Fighting.Helpers
     {
         public static int Count { get; } = 8;
 
-        public static Character[] GenerateCharacters()
+        public static Character[] GenerateCharacters(Side side)
         {
             Character[] characters = new Character[Count];
+            string s = side.ToString().ToLower();
 
             characters[0] = new Character
             {
                 Name = "Ken",
-                Image = Properties.Resources.Ken_left,
-                Head = Properties.Resources.Ken_left_1,
-                Body = Properties.Resources.Ken_left_2,
-                Legs = Properties.Resources.Ken_left_3,
+                Image = Properties.Resources.Ken_art,
+                Head = Properties.Resources.ResourceManager.GetObject($"Ken_{s}_1") as Image,
+                Body = Properties.Resources.ResourceManager.GetObject($"Ken_{s}_2") as Image,
+                Legs = Properties.Resources.ResourceManager.GetObject($"Ken_{s}_3") as Image,
             };
             characters[1] = new Character
             {
                 Name = "Ryu",
-                Image = Properties.Resources.Ryu_left,
-                Head = Properties.Resources.Ryu_right_1,
-                Body = Properties.Resources.Ryu_right_2,
-                Legs = Properties.Resources.Ryu_right_3,
+                Image = Properties.Resources.Ryu_art,
+                Head = Properties.Resources.ResourceManager.GetObject($"Ryu_{s}_1") as Image,
+                Body = Properties.Resources.ResourceManager.GetObject($"Ryu_{s}_2") as Image,
+                Legs = Properties.Resources.ResourceManager.GetObject($"Ryu_{s}_3") as Image,
             };
             characters[2] = new Character
             {
-                Name = "Kenn",
-                Image = Properties.Resources.Ken_left,
-                Head = Properties.Resources.Ken_left_1,
-                Body = Properties.Resources.Ken_left_2,
-                Legs = Properties.Resources.Ken_left_3,
+                Name = "Balrog",
+                Image = Properties.Resources.Barlog_art,
+                Head = Properties.Resources.ResourceManager.GetObject($"Balrog_{s}_1") as Image,
+                Body = Properties.Resources.ResourceManager.GetObject($"Balrog_{s}_2") as Image,
+                Legs = Properties.Resources.ResourceManager.GetObject($"Balrog_{s}_3") as Image,
             };
             characters[3] = new Character
             {
-                Name = "Ryuu",
-                Image = Properties.Resources.Ryu_left,
-                Head = Properties.Resources.Ryu_right_1,
-                Body = Properties.Resources.Ryu_right_2,
-                Legs = Properties.Resources.Ryu_right_3,
+                Name = "Guile",
+                Image = Properties.Resources.Guile_art,
+                Head = Properties.Resources.ResourceManager.GetObject($"Guile_{s}_1") as Image,
+                Body = Properties.Resources.ResourceManager.GetObject($"Guile_{s}_2") as Image,
+                Legs = Properties.Resources.ResourceManager.GetObject($"Guile_{s}_3") as Image,
             };
             characters[4] = new Character
             {
-                Name = "Kenn",
-                Image = Properties.Resources.Ken_left,
-                Head = Properties.Resources.Ken_left_1,
-                Body = Properties.Resources.Ken_left_2,
-                Legs = Properties.Resources.Ken_left_3,
+                Name = "Cammy",
+                Image = Properties.Resources.Cammy_art,
+                Head = Properties.Resources.ResourceManager.GetObject($"Cammy_{s}_1") as Image,
+                Body = Properties.Resources.ResourceManager.GetObject($"Cammy_{s}_2") as Image,
+                Legs = Properties.Resources.ResourceManager.GetObject($"Cammy_{s}_3") as Image,
             };
             characters[5] = new Character
             {
-                Name = "Ryuu",
-                Image = Properties.Resources.Ryu_left,
-                Head = Properties.Resources.Ryu_right_1,
-                Body = Properties.Resources.Ryu_right_2,
-                Legs = Properties.Resources.Ryu_right_3,
+                Name = "Chun-Li",
+                Image = Properties.Resources.ChunLi_art,
+                Head = Properties.Resources.ResourceManager.GetObject($"ChunLi_{s}_1") as Image,
+                Body = Properties.Resources.ResourceManager.GetObject($"ChunLi_{s}_2") as Image,
+                Legs = Properties.Resources.ResourceManager.GetObject($"ChunLi_{s}_3") as Image,
             };
             characters[6] = new Character
             {
-                Name = "Kenn",
-                Image = Properties.Resources.Ken_left,
-                Head = Properties.Resources.Ken_left_1,
-                Body = Properties.Resources.Ken_left_2,
-                Legs = Properties.Resources.Ken_left_3,
+                Name = "Juri",
+                Image = Properties.Resources.Juri_art,
+                Head = Properties.Resources.ResourceManager.GetObject($"Juri_{s}_1") as Image,
+                Body = Properties.Resources.ResourceManager.GetObject($"Juri_{s}_2") as Image,
+                Legs = Properties.Resources.ResourceManager.GetObject($"Juri_{s}_3") as Image,
             };
             characters[7] = new Character
             {
-                Name = "Ryuu",
-                Image = Properties.Resources.Ryu_left,
-                Head = Properties.Resources.Ryu_right_1,
-                Body = Properties.Resources.Ryu_right_2,
-                Legs = Properties.Resources.Ryu_right_3,
+                Name = "Sakura",
+                Image = Properties.Resources.Sakura_art,
+                Head = Properties.Resources.ResourceManager.GetObject($"Sakura_{s}_1") as Image,
+                Body = Properties.Resources.ResourceManager.GetObject($"Sakura_{s}_2") as Image,
+                Legs = Properties.Resources.ResourceManager.GetObject($"Sakura_{s}_3") as Image,
             };
             return characters;
         }
 
-        public static Character GenerateRandomCharacter()
+        public static Character GenerateRandomCharacter(Side side)
         {
-            return GenerateCharacters()[new Random().Next(Count)];
+            return GenerateCharacters(side)[new Random().Next(Count)];
+        }
+
+        public static Character GenerateRandomCharacterExcept(string name, Side side)
+        {
+            Character character;
+            do
+            {
+                character = GenerateCharacters(side)[new Random().Next(Count)];
+            }
+            while (character.Name == name);
+
+            return character;
         }
     }
 }
